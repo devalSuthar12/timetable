6d08b06b81622ba2400d674cd4f4ed20cef6575c
devalSuthar12 <22u1811@students.git.edu>
2025-01-04 22:13:17 +0530

    Create keys_dev.js

---
+++ time-table/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://root:pass1234@cluster0.rsri3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    secretOrKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1IiwidXNlcm5hbWUiOiJ0ZXN0dXNlciIsImVtYWlsIjoidGVzdHVzZXJAZXhhbXBsZS5jb20iLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTM1Mjg3NTksImV4cCI6MTY5MzUzMjM1OX0.O-GYn-DcJBBTT0pBWbO_MQ_uHjCH7WekHPgOnrE6Tjo"
+  };
